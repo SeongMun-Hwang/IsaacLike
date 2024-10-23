1c7341bdd7c33a799b0af988b9ce898ca4cc4845
hwang105814 <hwang105814@gmail.com>
2024-10-23 21:41:45 +0900

    Add Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject player;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+    }
+}
