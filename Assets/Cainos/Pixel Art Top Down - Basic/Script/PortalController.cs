802b1bd18f548a6bfbc53dd802b0aabeb5789f80
hwang105814 <hwang105814@gmail.com>
2024-10-31 17:37:00 +0900

    Add random enemy spawn

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/PortalController.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/PortalController.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class PortalController : MonoBehaviour
 {
+    //Portal
     public List<SpriteRenderer> renderers;
-    public List<GameObject> enemies;
+
+    //Enemy
+    public List<GameObject> enemyPrefabs; //복사할 몬스터 프레팹 리스트
+    public List<GameObject> enemies; //복사한 몬스터 저장 리스트
+    int minEnemy = 4;
+    int maxEnemy = 8;
 
     //Map
     public GameObject currentMap;
     public List<GameObject> maps;
 
+
+    
     void Update()
     {
         CheckExistingEnemy();
@@ -43,6 +51,22 @@ public class PortalController : MonoBehaviour
             }
         }
     }
+    private void SpawnMonster()
+    {
+        int rand=Random.Range(minEnemy,maxEnemy);
+        int rand1=Random.Range(0,rand);
+
+        for(int i = 0; i < rand1; i++)
+        {
+            GameObject go = Instantiate(enemyPrefabs[0]);
+            enemies.Add(go);
+        }
+        for (int i = 0; i < rand-rand1; i++)
+        {
+            GameObject go = Instantiate(enemyPrefabs[1]);
+            enemies.Add(go);
+        }
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.CompareTag("Player")&&Input.GetKeyDown(KeyCode.Space))
@@ -51,6 +75,7 @@ public class PortalController : MonoBehaviour
             int rand = Random.Range(0, 2);
             GameObject go = Instantiate(maps[rand]);
             currentMap = go;
+            SpawnMonster();
         }
     }
 }
